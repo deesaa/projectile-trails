9209e9bfdb056a9baea3f088a5a2b822e05d657b
deesaa <deemsaer@gmail.com>
2024-03-25 14:33:38 +0400

    fix: namespace

--- Assets/Example/ExampleProjectileTrailRenderer.cs
+++ Assets/Example/ExampleProjectileTrailRenderer.cs
@@ -4,53 +4,57 @@
 // Projectiles will be added and removed cyclically,
 // so you may expect projectile indices to be persistent.
 
-public class ExampleProjectileTrailRenderer : MonoBehaviour
+
+namespace TrailRenderer
 {
-    public Gun gun;
-    public TrailRenderer tracePrefab;
-    TrailRenderer[] _traces;
-    
-    private void Start()
+    public class ExampleProjectileTrailRenderer : MonoBehaviour
     {
-        if (gun != null)
+        public Gun gun;
+        public UnityEngine.TrailRenderer tracePrefab;
+        UnityEngine.TrailRenderer[] _traces;
+
+        private void Start()
         {
-            gun.onProjectileCreated += OnProjectileCreated;
-            gun.onProjectileRemoved += OnProjectileRemoved;
-            gun.onProjectileMoved += OnProjectileMoved;
+            if (gun != null)
+            {
+                gun.onProjectileCreated += OnProjectileCreated;
+                gun.onProjectileRemoved += OnProjectileRemoved;
+                gun.onProjectileMoved += OnProjectileMoved;
+            }
+
+            _traces = new UnityEngine.TrailRenderer[gun.maxProjectileCount];
+            for (int index = 0; index < _traces.Length; ++index)
+            {
+                UnityEngine.TrailRenderer trace = Instantiate(tracePrefab);
+                trace.gameObject.SetActive(false);
+                _traces[index] = trace;
+            }
         }
 
-        _traces = new TrailRenderer[gun.maxProjectileCount];
-        for (int index = 0; index < _traces.Length; ++index)
+        /// <param name="index">Unique numeric ID of a projectile in range [0, gun.maxProjectileCount - 1].</param>
+        /// <param name="projectile">The created projectile.</param>
+        private void OnProjectileCreated(int index, ref Gun.Projectile projectile)
         {
-            TrailRenderer trace = Instantiate(tracePrefab);
-            trace.gameObject.SetActive(false);
-            _traces[index] = trace;
+            _traces[index].transform.position = projectile.position;
+            _traces[index].Clear();
+            _traces[index].gameObject.SetActive(true);
         }
-    }
 
-    /// <param name="index">Unique numeric ID of a projectile in range [0, gun.maxProjectileCount - 1].</param>
-    /// <param name="projectile">The created projectile.</param>
-    private void OnProjectileCreated(int index, ref Gun.Projectile projectile)
-    {
-        _traces[index].transform.position = projectile.position;
-        _traces[index].Clear();
-        _traces[index].gameObject.SetActive(true);
-    }
+        /// <param name="index">Unique numeric ID of a projectile in range [0, gun.maxProjectileCount - 1].</param>
+        /// <param name="projectile">The removed projectile.</param>
+        private void OnProjectileRemoved(int index, ref Gun.Projectile projectile)
+        {
+            _traces[index].gameObject.SetActive(false);
+        }
 
-    /// <param name="index">Unique numeric ID of a projectile in range [0, gun.maxProjectileCount - 1].</param>
-    /// <param name="projectile">The removed projectile.</param>
-    private void OnProjectileRemoved(int index, ref Gun.Projectile projectile)
-    {
-        _traces[index].gameObject.SetActive(false);
-    }
-
-    /// <summary>
-    /// A callback that is called every frame while a projectile is moving.
-    /// </summary>
-    /// <param name="index">Unique numeric ID of a projectile in range [0, gun.maxProjectileCount - 1].</param>
-    /// <param name="projectile">The moved projectile.</param>
-    private void OnProjectileMoved(int index, ref Gun.Projectile projectile)
-    {
-        _traces[index].transform.position = projectile.position;
+        /// <summary>
+        /// A callback that is called every frame while a projectile is moving.
+        /// </summary>
+        /// <param name="index">Unique numeric ID of a projectile in range [0, gun.maxProjectileCount - 1].</param>
+        /// <param name="projectile">The moved projectile.</param>
+        private void OnProjectileMoved(int index, ref Gun.Projectile projectile)
+        {
+            _traces[index].transform.position = projectile.position;
+        }
     }
 }
