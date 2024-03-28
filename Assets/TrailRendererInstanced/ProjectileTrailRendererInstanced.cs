0382ed47ca060f665e1650610056021158363c88
deesaa <deemsaer@gmail.com>
2024-03-28 18:20:37 +0400

    upd: code cleanup

--- Assets/TrailRendererInstanced/ProjectileTrailRendererInstanced.cs
+++ Assets/TrailRendererInstanced/ProjectileTrailRendererInstanced.cs
@@ -1,21 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Rendering;
 
-namespace TrailRenderer
-{
-    public struct TrailRendererInstanced
-    {
-        public int meshSegmentsCount;
-        public Mesh trailMesh;
-        public Vector3[] vertices;
-        public int[] triangles;
-        public Vector2[] vertexUVs;
-        public int sumVerticesCount;
-        public int sumTrianglesCount;
-    }
-    
+namespace TrailRendererInstanced
+{ 
     public class ProjectileTrailRendererInstanced : MonoBehaviour
     {
         public Gun gun;
@@ -40,21 +27,15 @@ namespace TrailRenderer
         private static readonly int StartVelocityAndPassedTime = Shader.PropertyToID("_StartVelocityAndPassedTime");
         private static readonly int Gravity = Shader.PropertyToID("_Gravity");
         private static readonly int TrailShowTime = Shader.PropertyToID("_TrailShowTime");
-        
-        private float _maxTrailPathLength;
-        private float _simulationTimeDelta;
-        private int _maxTrailMeshSegments;
-        private int _initedRenderersCount;
-        private int _maxSumVerticesCount;
+        private static readonly int TrailLifeTime = Shader.PropertyToID("_TrailLifeTime");
+
         private bool _firstProjectileCreated;
-        private Vector2[] _vertexBufferUVs;
         private Mesh _meshInstance;
         private Material _materialInstance;
         private Matrix4x4[] _trailTransforms;
         private Vector4[] _startVelocitiesAndPassedTime;
         private MaterialPropertyBlock _materialPropertyBlock;
         private Camera _camera;
-        private static readonly int TrailLifeTime = Shader.PropertyToID("_TrailLifeTime");
 
         private void Start()
         {
@@ -63,17 +44,15 @@ namespace TrailRenderer
                 gun.onProjectileCreated += OnProjectileCreated;
                 gun.onProjectileMoved += OnProjectileMoved;
             
-                _maxTrailPathLength = gun.startingVelocity * gun.lifetime + 
+                var maxTrailPathLength = gun.startingVelocity * gun.lifetime + 
                                       ((Physics.gravity.magnitude * gun.lifetime * gun.lifetime) * 0.5f);
-                _maxTrailMeshSegments = Mathf.CeilToInt(_maxTrailPathLength / segmentLength);
-                _simulationTimeDelta = gun.lifetime / _maxTrailMeshSegments;
-                _maxSumVerticesCount = 2 + (_maxTrailMeshSegments * 2);
+                var maxTrailMeshSegments = Mathf.CeilToInt(maxTrailPathLength / segmentLength);
+                var simulationTimeDelta = gun.lifetime / maxTrailMeshSegments;
                 _trailTransforms = new Matrix4x4[gun.maxProjectileCount];
                 _startVelocitiesAndPassedTime = new Vector4[gun.maxProjectileCount];
-                _vertexBufferUVs = new Vector2[_maxSumVerticesCount];
                 _materialPropertyBlock = new MaterialPropertyBlock();
                 _materialPropertyBlock.SetVectorArray(StartVelocityAndPassedTime, _startVelocitiesAndPassedTime);
-                InitMesh(_maxTrailMeshSegments, _simulationTimeDelta);
+                InitMesh(maxTrailMeshSegments, simulationTimeDelta);
                 _camera = Camera.main;
             }
         }
@@ -120,37 +99,35 @@ namespace TrailRenderer
         
         private void InitMesh(int meshSegmentsCount, float projectileSimulationDeltaTime)
         {
-            var newRenderer = new ProjectileTrailRenderersPool.SingleTrailRenderer();
-            newRenderer.sumVerticesCount = 2 + (meshSegmentsCount * 2);
-            newRenderer.sumTrianglesCount = (meshSegmentsCount * 6);
-            newRenderer.trailMesh = new Mesh();
-            newRenderer.vertices = new Vector3[newRenderer.sumVerticesCount];
-            newRenderer.triangles = new int[newRenderer.sumTrianglesCount];
-            newRenderer.vertexUVs = new Vector2[newRenderer.sumVerticesCount];
+            var sumVerticesCount = 2 + (meshSegmentsCount * 2);
+            var sumTrianglesCount = (meshSegmentsCount * 6);
+            var trailMesh = new Mesh();
+            var vertices = new Vector3[sumVerticesCount];
+            var triangles = new int[sumTrianglesCount];
+            var vertexUVs = new Vector2[sumVerticesCount];
             
-            for (int i = 0, ti = 0; ti < newRenderer.sumTrianglesCount; i += 2, ti += 6)
+            for (int i = 0, ti = 0; ti < sumTrianglesCount; i += 2, ti += 6)
             {
-                newRenderer.triangles[ti] = i;
-                newRenderer.triangles[ti+1] = i + 2;
-                newRenderer.triangles[ti+2] = i + 1;
-                newRenderer.triangles[ti+3] = i + 1;
-                newRenderer.triangles[ti+4] = i + 2;
-                newRenderer.triangles[ti+5] = i + 3;
+                triangles[ti] = i;
+                triangles[ti+1] = i + 2;
+                triangles[ti+3] = i + 1;
+                triangles[ti+2] = i + 1;
+                triangles[ti+4] = i + 2;
+                triangles[ti+5] = i + 3;
             }
 
             float simulationTime = 0f;
             for (int i = 0, vi = 0; i <= meshSegmentsCount; i++, vi += 2)
             {
-                _vertexBufferUVs[vi] = new Vector2(simulationTime, -1f);
-                _vertexBufferUVs[vi + 1] = new Vector2(simulationTime, 1f);
+                vertexUVs[vi] = new Vector2(simulationTime, -1f);
+                vertexUVs[vi + 1] = new Vector2(simulationTime, 1f);
                 simulationTime += projectileSimulationDeltaTime;
             }
 
-            newRenderer.trailMesh.vertices = newRenderer.vertices;
-            newRenderer.trailMesh.triangles = newRenderer.triangles;
-            newRenderer.trailMesh.SetUVs(0, _vertexBufferUVs);
-            newRenderer.meshSegmentsCount = meshSegmentsCount;
-            _meshInstance = newRenderer.trailMesh;
+            trailMesh.vertices = vertices;
+            trailMesh.triangles = triangles;
+            trailMesh.SetUVs(0, vertexUVs);
+            _meshInstance = trailMesh;
         }
     }
 }
